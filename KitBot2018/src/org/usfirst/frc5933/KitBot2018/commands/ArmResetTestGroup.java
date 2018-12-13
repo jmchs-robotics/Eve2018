9c54ec4fafb68abb8db106c5aac0d0c36d7d0328
Jon Hall <jhjh87@gmail.com>
2018-12-12 17:22:31 -0700

    autonomous drive straight 38.5'

--- KitBot2018/src/org/usfirst/frc5933/KitBot2018/commands/ArmResetTestGroup.java
+++ KitBot2018/src/org/usfirst/frc5933/KitBot2018/commands/ArmResetTestGroup.java
@@ -26,8 +26,27 @@ public class ArmResetTestGroup extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+
+    	testHere();
+    	// testResettingArm();
+    }
+    
+    private void testHere() {
+      	
+    	// first test set, 36 desired: 49.5 traveled; second run 51; third 54; fourth 50. so multiply (desired inches)  by (36 / 50)
+    	// second test set, 72 desired: 84 traveled; second run 87; third 85
+    	// third test set, 240 desired: 256 traveled; second run 260; third 259 [five seconds]
+    	// fourth: 28' tried as 28*12*240/258, 28' 6" traveled
     	
-    	testResettingArm();
+    	// drive 28+8.5+2' to take cube into alcove
+    	addSequential( new DriveStraightGyro( (28*12 +10.5*12)*240/258, 0.7, true)); 
+
+    	// back up 4' or so 
+    //	addSequential( new DriveStraightGyro( -36, -0.7, true)); 
+    	
+      	// test turn: first test 63 for 90; 2nd test 8deg too short; 
+    //	addSequential( new GyroTurn( -66.0, 0.7, 0.05));
+
     }
     
     private void testResettingArm() {
